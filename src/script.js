4456abd6b6f68903a773beafe76308302ec41ee2
Arthur Jenck <arthurjenckdev@gmail.com>
2026-01-26 11:21:44 +0100

    docker and deps stup

---
+++ src/script.js
@@ -0,0 +1,112 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { GUI } from 'lil-gui'
+
+/**
+ * GUI
+ */
+const gui = new GUI()
+
+/**
+ * Canvas
+ */
+const canvas = document.querySelector('canvas.webgl')
+
+const scene = new THREE.Scene()
+scene.background = new THREE.Color('#6887ed')
+
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+}
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(
+    60,
+    sizes.width / sizes.height,
+    0.1,
+    1550,
+)
+
+camera.position.set(50, 40, 40)
+camera.lookAt(0, 0, 0)
+
+scene.add(camera)
+
+/**
+ * Objects
+ */
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(20, 20, 20),
+    // new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true }),
+    new THREE.MeshPhongMaterial({ color: 'red' }),
+)
+
+scene.add(cube)
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 10)
+directionalLight.position.set(0, 25, 0)
+scene.add(directionalLight)
+
+const directionalLightHelper = new THREE.DirectionalLightHelper(
+    directionalLight,
+)
+scene.add(directionalLightHelper)
+
+const directionalLightFolder = gui.addFolder('Directional light')
+directionalLightFolder.addColor(directionalLight, 'color')
+directionalLightFolder
+    .add(directionalLight.position, 'x')
+    .min(-25)
+    .max(25)
+    .step(0.001)
+directionalLightFolder
+    .add(directionalLight.position, 'y')
+    .min(-25)
+    .max(25)
+    .step(0.001)
+directionalLightFolder
+    .add(directionalLight.position, 'z')
+    .min(-25)
+    .max(25)
+    .step(0.001)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true,
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+renderer.render(scene, camera)
+
+/**
+ * Controls
+ */
+const controls = new OrbitControls(camera, renderer.domElement)
+
+/**
+ * Tick
+ */
+const clock = new THREE.Clock()
+
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
+    controls.update()
+
+    renderer.render(scene, camera)
+    requestAnimationFrame(tick)
+}
+
+requestAnimationFrame(tick)
